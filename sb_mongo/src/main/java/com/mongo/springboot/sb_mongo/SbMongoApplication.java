2b55710d20a2d49f007e4fd9d3c1ebed2ed11b94
Kamal pant <kamalpant07@gmail.com>
2023-04-16 00:23:17 +0530

    first commit

---
+++ sb_mongo/src/main/java/com/mongo/springboot/sb_mongo/SbMongoApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.springboot.sb_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMongoApplication.class, args);
+	}
+
+}
